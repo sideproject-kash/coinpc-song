9875122d1288a411d467ec647a390eb47e2def52
taeheon.song <taeheon.song@linecorp.com>
2022-09-27 08:44:39 +0900

    [sth-2] change dir

--- deploy/run_process.sh
+++ deploy/run_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'run process'
-cd /home/ec2-user/test
-nohup java -jar coinpc-0.0.1-SNAPSHOT.jar > /home/ec2-user/test/springboot.log 2>&1 &
+cd /home/ec2-user/coinpc
+nohup java -jar coinpc-0.0.1-SNAPSHOT.jar > /home/ec2-user/coinpc/springboot.log 2>&1 &
 echo 'run process done'
