6c28fe0d74d1d3626fcbfad6d63fed661c52068d
taeheon.song <taeheon.song@linecorp.com>
2022-09-29 09:35:42 +0900

    [sth-2] limit java memory

--- deploy/run_process.sh
+++ deploy/run_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'run process'
 cd /home/ec2-user/coinpc
-nohup java -jar coinpc-0.0.1-SNAPSHOT.jar > /home/ec2-user/coinpc/springboot.log 2>&1 &
+nohup java -jar coinpc-0.0.1-SNAPSHOT.jar -Xms64m -Xm64m > /home/ec2-user/coinpc/springboot.log 2>&1 &
 echo 'run process done'
